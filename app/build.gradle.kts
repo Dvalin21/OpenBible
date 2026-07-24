d7e7676b26c6dde0c7e9e90fd8cda7e7b06e46c3
Keith <keith@openbible.dev>
2026-07-24 01:38:51 -0500

    Bump version to 1.2.9 (versionCode 11)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.openbible"
         minSdk = 29
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.2.8"
+        versionCode = 11
+        versionName = "1.2.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
